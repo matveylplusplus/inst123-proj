05554d8746bb1832a42a62b474ab9f2a01fc2e7d
Matvey Lyapin <87352866+matveylplusplus@users.noreply.github.com>
2023-11-14 14:37:24 -0500

    Update main.sql

--- main.sql
+++ main.sql
@@ -1,27 +1,13 @@
-
 CREATE DATABASE musik;
-
 
 CREATE TYPE ATYPE AS ENUM
 ('Solo','Group','Band','Collective','Other')
 ;
-
-
 CREATE TABLE artists (
 	name varchar(50),
 	type ATYPE,
 	PRIMARY KEY (name)
 );
-
-
-INSERT INTO artists (name, type)
-	VALUES ('Pink Floyd','Band'),
-	('Porter Robinson','Solo'),
-	('Drake','Solo')
-;
-
-
-
 
 CREATE TABLE genres (
     genre_name TEXT CONSTRAINT genres_key PRIMARY KEY,
@@ -33,3 +19,8 @@ INSERT INTO genres (genre_name, genre_desc)
         ('rock', 'a type of popular dance music originating in the 1950s, characterized by heavy beats and uncomplicated melodies; rock first emerged as a fusion between R&B and country music'),
         ('dance/electronic', 'a broad genre of percussive electronic music intended for dancing; originally made for nightclubs, raves, and festivals'),
         ('hip-hop/rap', 'a genre of music characterized primarily by a strong, rhythmic beat and rapping vocals');
+
+INSERT INTO artists (name, type)
+	VALUES ('Pink Floyd','Band'),
+	('Porter Robinson','Solo'),
+	('Drake','Solo');
