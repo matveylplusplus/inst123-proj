ae54d6f602d6b1e83e21f0bdf51ab53d0b26d258
SSD Matt <matthecrazyguy@gmail.com>
2023-11-14 15:09:37 -0500

    slight tab changes

--- main.sql
+++ main.sql
@@ -23,14 +23,13 @@ CREATE TABLE releases (
 );
 
 INSERT INTO genres (genre_name, genre_desc) VALUES
-        ('rock', 'a type of popular dance music originating in the 1950s, 
-         characterized by heavy beats and uncomplicated melodies; 
-         rock first emerged as a fusion between R&B and country music'),
-        ('dance/electronic', 'a broad genre of percussive electronic 
-         music intended for dancing; originally made for nightclubs, raves,
-         and festivals'),
-        ('hip-hop/rap', 'a genre of music characterized primarily by a 
-         strong, rhythmic beat and rapping vocals');
+  ('rock', 'a type of popular dance music originating in the 1950s, 
+  characterized by heavy beats and uncomplicated melodies; rock first emerged as
+  a fusion between R&B and country music'),
+  ('dance/electronic', 'a broad genre of percussive electronic music intended 
+  for dancing; originally made for nightclubs, raves, and festivals'),
+  ('hip-hop/rap', 'a genre of music characterized primarily by a strong, 
+  rhythmic beat and rapping vocals');
 
 INSERT INTO releases
 (title, artist, type, track_count, duration, release_date, 
