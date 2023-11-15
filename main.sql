e04c424ff1ead3ddad1a6beb039c94729e6de0aa
SSD Matt <matthecrazyguy@gmail.com>
2023-11-14 21:09:40 -0500

    removed grammy attribute because I realized it's stupid, changed a few attribute names for clarity, added NULL checks, and undid genres sentence breaks because apparently thats bad

--- main.sql
+++ main.sql
@@ -8,42 +8,35 @@ CREATE TABLE genres (
 CREATE TYPE RTYPE AS ENUM
 ('Single', 'Album', 'EP', 'Other');
 CREATE TABLE releases (
-  title TEXT,
-  artist TEXT REFERENCES artists(name),
-  PRIMARY KEY(title, artist),
-  type RTYPE,
-  track_count INT CHECK (track_count > 0),
-  duration INTERVAL CHECK (EXTRACT(epoch FROM duration) > 0),
+  release_title TEXT,
+  artist_name TEXT REFERENCES artists(artist_name),
+  PRIMARY KEY(release_title, artist_name),
+  release_type RTYPE NOT NULL,
+  track_count INT NOT NULL CHECK (track_count > 0),
+  duration INTERVAL NOT NULL CHECK (EXTRACT(epoch FROM duration) > 0),
   release_date DATE,
-  genre TEXT REFERENCES genres(genre_name),
+  genre_name TEXT NOT NULL REFERENCES genres(genre_name),
   peak_chart_pos_us INT CHECK (peak_chart_pos_us > 0),
   peak_chart_pos_uk INT CHECK (peak_chart_pos_uk > 0),
   am_rating NUMERIC(2,1) CHECK (am_rating BETWEEN 0 AND 5),
   rym_rating NUMERIC(3,2) CHECK (rym_rating BETWEEN 0 AND 5),
-  pitchfork_rating NUMERIC (3,1) CHECK (pitchfork_rating BETWEEN 0 AND 10),
-  gram_nom BOOLEAN,
-  gram_win BOOLEAN
+  pitchfork_rating NUMERIC (3,1) CHECK (pitchfork_rating BETWEEN 0 AND 10)
 );
 
 INSERT INTO genres (genre_name, genre_desc) VALUES
-  ('rock', 'a type of popular dance music originating in the 1950s, 
-  characterized by heavy beats and uncomplicated melodies; rock first emerged as
-  a fusion between R&B and country music'),
-  ('dance/electronic', 'a broad genre of percussive electronic music intended 
-  for dancing; originally made for nightclubs, raves, and festivals'),
-  ('hip-hop/rap', 'a genre of music characterized primarily by a strong, 
-  rhythmic beat and rapping vocals');
+  ('rock', 'a type of popular dance music originating in the 1950s, characterized by heavy beats and uncomplicated melodies; rock first emerged asa fusion between R&B and country music'),
+  ('dance/electronic', 'a broad genre of percussive electronic music intended for dancing; originally made for nightclubs, raves, and festivals'),
+  ('hip-hop/rap', 'a genre of music characterized primarily by a strong, rhythmic beat and rapping vocals');
 
 INSERT INTO releases
-(title, artist, type, track_count, duration, release_date, 
-genre, peak_chart_pos_us, peak_chart_pos_uk, am_rating, 
-rym_rating, pitchfork_rating, gram_nom, gram_win) VALUES
+(release_title, artist_name, release_type, track_count, duration, release_date, 
+genre_name, peak_chart_pos_us, peak_chart_pos_uk, am_rating, 
+rym_rating, pitchfork_rating) VALUES
   ('The Dark Side of the Moon', 'Pink Floyd', 'Album', 
   10, '00:42:50', '1973-03-23', 'rock', 1, 2, 5.0, 4.23,
-  9.3, FALSE, FALSE),
+  9.3),
   ('Worlds', 'Porter Robinson', 'Album', 12, '00:57:49', 
   '2014-08-12', 'dance/electronic', 18, 13, 4.0, 3.55,
-  6.9, FALSE, FALSE),
+  6.9),
   ('Certified Lover Boy', 'Drake', 'Album', 21, '01:26:02',
-  '2021-09-03', 'hip-hop/rap', 1, 1, 2.5, 1.78, 6.6, 
-  FALSE, FALSE);
+  '2021-09-03', 'hip-hop/rap', 1, 1, 2.5, 1.78, 6.6);
